8e653050922ec600ad4bf26e0004f21a8de70019
Artur Logan <arturlogan1ferreira@gmail.com>
2025-08-20 17:17:44 -0300

    Primeiro commit da api gateway

---
+++ api-gateway-estudo/src/main/java/com/arturlogan/api_gateway_estudo/ApiGatewayEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.api_gateway_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayEstudoApplication.class, args);
+	}
+
+}
---
+++ api-gateway-estudo/src/test/java/com/arturlogan/api_gateway_estudo/ApiGatewayEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.api_gateway_estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
